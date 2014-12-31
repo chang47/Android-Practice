374983ff33b521374860cf50adcf68065bf5025b
chang47 <coralbue@gmail.com>
2014-12-30 21:05:54 -0800

    using new activities

--- HelloWorld/app/src/main/java/com/coralbue/helloworld/DisplayMessageActivity.java
+++ HelloWorld/app/src/main/java/com/coralbue/helloworld/DisplayMessageActivity.java
@@ -5,23 +5,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
-    public static final String EXTRA_MESSAGE = "com.coralbue.helloworld.MESSAGE";
+public class DisplayMessageActivity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //makes a text view
+        TextView tv = new TextView(this);
+        tv.setTextSize(40);
+        tv.setText(message);
+
+        //adds text view to the current activity
+        setContentView(tv);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
         return true;
     }
 
@@ -38,13 +46,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
