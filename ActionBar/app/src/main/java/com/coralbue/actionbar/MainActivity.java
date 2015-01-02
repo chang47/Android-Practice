c5ac90226ea9a32cd34b45b0cb3b0d465cb8bd93
chang47 <coralbue@gmail.com>
2015-01-01 22:26:55 -0800

    actionbar exploration

--- ActionBar/app/src/main/java/com/coralbue/actionbar/MainActivity.java
+++ ActionBar/app/src/main/java/com/coralbue/actionbar/MainActivity.java
@@ -1,10 +1,12 @@
 package com.coralbue.actionbar;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -29,13 +31,27 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_search:
+                return true;
+            case R.id.action_location_found:
+                LocationFound();
+                return true;
+            case R.id.action_refresh:
+                //refresh
+            case  R.id.action_help:
+                //
+                return true;
+            case R.id.action_check_updates:
+                //
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void LocationFound() {
+        Intent i = new Intent(MainActivity.this, LocationFound.class);
+        startActivity(i);
     }
 }
--- HelloWorld/app/src/main/java/com/coralbue/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/coralbue/helloworld/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_activity_action, menu);
+        inflater.inflate(R.menu.main_activity_actions, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
