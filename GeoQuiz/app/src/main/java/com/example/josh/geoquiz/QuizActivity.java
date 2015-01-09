72436fe79b474eabfc9532596fa197eeabca76c6
chang47 <coralbue@gmail.com>
2015-01-08 21:33:57 -0800

    finished quiz app, added next app

--- GeoQuiz/app/src/main/java/com/example/josh/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/josh/geoquiz/QuizActivity.java
@@ -1,6 +1,9 @@
 package com.example.josh.geoquiz;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -56,12 +59,17 @@ public class QuizActivity extends ActionBarActivity {
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
-
+    @TargetApi(11)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            actionBar.setSubtitle("Bodies of water!");
+        }
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
